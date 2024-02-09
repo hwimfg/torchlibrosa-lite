717755084a532493fc5316bca8053d7b52be4f63
Kyle Cooper <kcooper@hwimfg.com>
2024-02-09 14:30:35 -0700

    Implement get_window, add scipy dependency

--- torchlibrosa/stft.py
+++ torchlibrosa/stft.py
@@ -3,6 +3,7 @@ import argparse
 
 # import librosa
 import numpy as np
+import scipy
 
 import torch
 import torch.nn as nn
@@ -10,7 +11,69 @@ import torch.nn.functional as F
 from torch.nn.parameter import Parameter
 
 class LibrosaLite():
-    def filters_get_window(self, window, win_length, fftbins=False):
+    def filters_get_window(self, window, Nx, *, fftbins=True):
+        """Compute a window function.
+
+        This is a wrapper for `scipy.signal.get_window` that additionally
+        supports callable or pre-computed windows.
+
+        Parameters
+        ----------
+        window : string, tuple, number, callable, or list-like
+            The window specification:
+
+            - If string, it's the name of the window function (e.g., `'hann'`)
+            - If tuple, it's the name of the window function and any parameters
+            (e.g., `('kaiser', 4.0)`)
+            - If numeric, it is treated as the beta parameter of the `'kaiser'`
+            window, as in `scipy.signal.get_window`.
+            - If callable, it's a function that accepts one integer argument
+            (the window length)
+            - If list-like, it's a pre-computed window of the correct length `Nx`
+
+        Nx : int > 0
+            The length of the window
+
+        fftbins : bool, optional
+            If True (default), create a periodic window for use with FFT
+            If False, create a symmetric window for filter design applications.
+
+        Returns
+        -------
+        get_window : np.ndarray
+            A window of length `Nx` and type `window`
+
+        See Also
+        --------
+        scipy.signal.get_window
+
+        Notes
+        -----
+        This function caches at level 10.
+
+        Raises
+        ------
+        ParameterError
+            If `window` is supplied as a vector of length != `n_fft`,
+            or is otherwise mis-specified.
+        """
+        if callable(window):
+            return window(Nx)
+
+        elif isinstance(window, (str, tuple)) or np.isscalar(window):
+            # TODO: if we add custom window functions in librosa, call them here
+
+            win: np.ndarray = scipy.signal.get_window(window, Nx, fftbins=fftbins)
+            return win
+
+        elif isinstance(window, (np.ndarray, list)):
+            if len(window) == Nx:
+                return np.asarray(window)
+
+            raise Exception(f"Window size mismatch: {len(window):d} != {Nx:d}")
+        else:
+            raise Exception(f"Invalid window specification: {window!r}")
+
         
     def util_pad_center(self, window, n_fft):
         
