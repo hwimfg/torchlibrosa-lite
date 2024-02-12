ccb1188c349295f1cbceec47271d237fc061e5f0
Kyle Cooper <kcooper@hwimfg.com>
2024-02-12 15:47:07 -0700

    Expose librosalite in init.py

--- torchlibrosa/__init__.py
+++ torchlibrosa/__init__.py
@@ -4,5 +4,6 @@ import torchlibrosa.stft
 from torchlibrosa.augmentation import DropStripes, SpecAugmentation
 from torchlibrosa.stft import DFTBase, DFT, STFT, ISTFT, Spectrogram, \
 	LogmelFilterBank, Enframe, Scalar
+from torchlibrosa.librosalite import LibrosaLite
 
 __version__ = '0.0.9'
